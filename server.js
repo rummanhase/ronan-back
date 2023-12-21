90af1aedc746841f306ce211fd2ef314e0904c76
rummanhase <rummanhase@gmail.com>
2023-12-21 10:08:13 +0530

    cors issue

--- server.js
+++ server.js
@@ -49,13 +49,12 @@ let corsOptions = {
   origin: ["http://localhost:3000", "http://localhost:3001/", "http://localhost:3001" , "http://localhost:3000/" , "https://ronan-fashion-back.onrender.com" , "https://ronan-fashion-back.onrender.com/"],
 };
 
-app.use(cors(corsOptions));
-
-app.use(express.static('uploads'));
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: false
-}));
+const app = express();
+app.use(cors({
+        origin: "https://ronan-fashion-back.onrender.com"
+    }
+))
+app.options('*', cors())
 
 app.use("/", (req, res, next) => {
   try {
